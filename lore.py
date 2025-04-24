d9d64b3b14864652e4673866ee4fcd92b36d3bd9
matheus silva <matheus.tads.code@gmail.com>
2025-04-23 23:46:10 -0300

    Lore do game

---
+++ lore.py
@@ -0,0 +1,16 @@
+def mostrar_lore():
+    lore = """
+
+    🌍 Bem Vindo ao mundo de Caos Ordenado!!!
+    
+    Em um mundo onde a magia e a tecnlogia existe, as leis universais nao sao mais
+    divinas, sao habilidades moldadas por humanos.
+
+    Voce, Haruki Dōro, um ser que controla as regras do universo com sinais, codigos e barreiras,
+    procura proteger o mundo e garantir a instabilidade com suas habilidadades, sua filosofia sempre foi 
+    proteger os mais fracos e vulneraveis
+
+    Por outro lado... existe o mal, um ser que nasceu a partir de erro qunatico fatal, onde ele usa seus poderes, controlando o acaso 
+    e a aletoriedade, causando rupturas na realidade, colocando a vida de todos em perigo 
+
+"""
